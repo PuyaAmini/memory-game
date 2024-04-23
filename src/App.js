4bbcec078ad29447ae2f4a5c9a9c16231a166211
pamini <puyagit@gmail.com>
2024-04-23 11:24:38 +0330

    [*retrain] shuffling cards fun added then shuffled imgs added in a [] then mapped in dom

--- src/App.js
+++ src/App.js
@@ -14,12 +14,36 @@ const cardImages = [
 
 function App() {
 
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
 
+  const shuffleCards = () => {
+    
+    const shuffled_Cards = [...cardImages, ...cardImages]
+    .sort(() => Math.random()-0.5)
+    .map(card => ({...card , id: Math.random()}))
+
+    setCards(shuffled_Cards)
+    setTurns(0)
+  }
+
+  useEffect(() => {
+    console.log(cards , turns)
+  } , [cards , turns])
 
 
   return (
     <div className="App">
-
+      <h1>Magic Mind</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className='card-grid'>{cards.map(card => (
+        <div className='card' key={card.id}>
+          <div>
+            <img className='front' src={card.src} alt='card front'/>
+            <img className='back' src='/img/cover.png' alt='card back'/>
+          </div>
+        </div>
+      ))}</div>
     </div>
   );
 }
